fd6410d0f8ae675995c9e1f47fc9eb041abe5452
n17011 <n17011@std.it-college.ac.jp>
2018-12-05 14:00:10 +0900

    add new file

---
+++ app.js
@@ -0,0 +1,57 @@
+//APIキーの設定とSDKの初期化
+var appKey    = ;
+var clientKey = ;
+var ncmb    　= new NCMB(appKey,clientKey);
+
+// -------[Demo1]データをmBaaSに保存する -------//
+function sendForm() {
+        
+    //ユーザーの入力したデータを変数にセットする
+    var text    = $("#form_text").val();               //予定
+    var time = $("#form_time").val();                   //時間
+    var textarea  = $("#form_textarea").val();          //メモ書き
+    var listA   = $("#form_listA").val();           //かてごりーA 
+    var moneyA  = $("#form_moneyA").val();              //金額A
+    var listB   = $("#form_listB").val();           //かてごりーB
+    var moneyB  = $("#form_moneyB").val();              //金額B        
+    
+    //time,moneyA,moneyBを数値に変換
+    var time = Number(time);
+    var moneyA = Number(moneyA);
+    var moneyB = Number(moneyB);
+
+        
+    //入力規則およびデータをフィールドにセットする
+    if(text == ""){
+        alert("予定が入力されていません");
+    }else if(time == ""){
+        alert("時間が入力されていません");
+    }else{
+        //mBaaSに保存先クラスの作成
+        var SaveData = ncmb.DataStore("SaveData");
+            
+        //インスタンスの生成
+        var saveData = new SaveData();
+            
+        //インスタンスにデータをセットする
+        saveData.set("text", text)
+                .set("time", time)
+                .set("textarea", textarea)
+                .set("income", listA)
+                .set("moneyA", moneyA)
+                .set("spending", listB)
+                .set("moneyB", moneyB)
+                .save()
+                .then(function(results){
+                    //保存に成功した場合の処理
+                    alert("登録しました");
+                    console.log("登録成功");
+                    location.reload();
+                })
+                .catch(function(error){
+                    //保存に失敗した場合の処理
+                    alert("登録に失敗しました：\n" + error);
+                    console.log("登録失敗：\n" + error);
+                });
+    }
+}
