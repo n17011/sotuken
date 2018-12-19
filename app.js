7957faccc5aa3e3304fe2ef38296ae68ef15a5b6
n17011 <n17011@std.it-college.ac.jp>
2018-12-19 16:24:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-g//APIキーの設定とSDKの初期化
-var appKey    = ;
-var clientKey = ;
+//APIキーの設定とSDKの初期化
+var appKey    = "9aa7b3e3d9c2f85aa3093bdf734eef5b98f357e55c2191ac621871fdbf757430";
+var clientKey = "c2c0df4210406c0b45155c416e7a8a4ce4df98124ea8dbe3f72342d53785538d";
 var ncmb    　= new NCMB(appKey,clientKey);
 
-// -------[Demo1]データをmBaaSに保存する -------//
 function sendForm() {
         
     //ユーザーの入力したデータを変数にセットする
@@ -16,10 +15,9 @@ function sendForm() {
     var listB   = $("#form_listB").val();           //かてごりーB
     var moneyB  = $("#form_moneyB").val();              //金額B        
     
-    //time,moneyA,moneyBを数値に変換
-    var time = Number(time);
-    var moneyA = Number(moneyA);
-    var moneyB = Number(moneyB);
+    //金額から","を取ってテキストに戻す
+    var moneyA = parseInt(moneyA.split(',').join('').trim());
+    var moneyB = parseInt(moneyB.split(',').join('').trim());
 
         
     //入力規則およびデータをフィールドにセットする
@@ -82,40 +80,58 @@ function checkForm(){
             });
 }
 
-function checkDate(){ 
+function checkDate(divider){
     //データを変数にセット
-    var searchdate = $('#search_date').val();
+    var datetime  = $("#search_date").val();
         
     //インスタンスの生成
-    var saveData = ncmb.DataStore("SaveData");
+    var saveData  = ncmb.DataStore("SaveData");
         
-    //データの取得 
-    saveData.order("createDate",true)
-            .equalTo("searchdate",searchdate)
-            .fetchAll()
-            .then(function(results){
-                //日付の検索に成功した場合の処理
-                console.log("日付の検索に成功しました："+results.length+"件");
-                setData(results);
-                $.mobile.changePage('#ListUpPage');
-            })
-            .catch(function(error){
-                //日付の検索に失敗した場合の処理
-                alert("日付の検索に失敗しました：\n" + error);
-                console.log("日付の検索に失敗しました：\n" + error);
-            });
+                saveData.order("createDate",true)
+                        .equalTo("datetime",datetime)
+                        .fetchAll()
+                        .then(function(results){
+                            //日付の検索に成功した場合の処理
+                            console.log("日付の検索に成功しました："+results.length+"件");
+                            setData(results);
+                        })
+                        .catch(function(error){
+                            //日付の検索に失敗した場合の処理
+                            alert("日付の検索に失敗しました：\n" + error);
+                            console.log("日付の検索に失敗しました：\n" + error);
+                        });
 }
-
+var income = [];
+var moneyA = [];
+var spending = [];
+var moneyB = [];
 function setData(results) {
     //操作するテーブルへの参照を取得
     var table = document.getElementById("formTable");
         for(i=0; i<results.length; i++) {
+            var object   = results[i];
+            var year     = object.get("createDate").slice(0,4);      //YYYYを取り出す
+            var month    = object.get("createDate").slice(5,7);      //MMを取り出す
+            var day      = object.get("createDate").slice(8,10);     //DDを取り出す            
+            var hour     = object.get("createDate");                 //hhを取り出す
+            var minute   = object.get("createDate").slice(14,16);    //mmを取り出す
+                
+            //hourが協定時間なので、現地時間（+09:00）となるようにする
+            var datehour = new Date(hour);  //hourをDate型に変換
+            var jsthour  = datehour.getHours();  //datehourを現地時間にする
+            var jstDate  = "入力した時間：" +　year + "/" + month + "/" + day + " " + jsthour +":"+ minute;
+                
             //テーブルに行とセルを設定
             var row      = table.insertRow(-1);
             var cell     = row.insertCell(-1);
                 
-            formTable.rows[i].cells[0].innerHTML = jstDate + "<br>" + "日付：　" + object.get("datetime") + "<br>" +"予定："+object.get("text")+ "予定時間：　" + object.get("time");
+            formTable.rows[i].cells[0].innerHTML = jstDate + "<br>" + "日時：" + object.get("datetime")+"<br>" +"予定："+object.get("text")+ "<br>"+"予定時間：" + object.get("time") + "<br>"+"収入金額：" + object.get("moneyA")+"<br>"+"支出金額：" + object.get("moneyB");
+            moneyA[i] = object.get("moneyA")
+            income[i] = object.get("income")
+            moneyB[i] = object.get("moneyB")
+            spending[i] = object.get("spending")
         }
+       
     var searchResult = document.getElementById("searchResult");
     searchResult.innerHTML = "検索結果："+results.length+"件";
         
@@ -130,15 +146,15 @@ function setData(results) {
 //日付検索ボタン押下時の処理
 function searchDate(){
     $("#formTable").empty();
-    var searchdate  = $("#search_date").val();
+    var datetime = $("#search_date").val();
         
         
     //Date型に変換
-    var date = new Date(searchDate);
+    var date = new Date(datetime);
         
     //フィールドの中から探す
-    if(searchdate == ""){
+    if(datetime == ""){
         alert("年月日を入力してください");                
     }else
-        checkDate(searchdate);
+        checkDate(datetime);
 }
